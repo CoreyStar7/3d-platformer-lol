49030e9f4b2c3526df47fb414b665afa17c554b1
CoreyStar7 <64766411+CoreyStar7@users.noreply.github.com>
2021-03-31 10:25:16 +1100

    Add files via upload

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Variables
+    public Transform target;
+
+    public Vector3 offset;
+
+    public bool useOffsetValues;
+
+    public float rotateSpeed;
+
+    public Transform pivot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(!useOffsetValues) // if NOT useOffsetValues then set the offset based on how far the Camera and Character is at the start of the game ("!" means not)
+        { 
+        offset = target.position - transform.position; // Set the camera at the start of the session
+        }
+
+        pivot.transform.position = target.transform.position; // Move the pivot to wherever the player is on Game Boot
+        pivot.transform.parent = target.transform; // Changing parents from Main Camera to Player on Game Boot
+
+        Cursor.lockState = CursorLockMode.Locked; // Hide the cursor pointer on Game Boot
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed; // Get the X Axis of the Computer's Attached Physical Mouse and Rotate the Camera Target
+        target.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        pivot.Rotate(-vertical, 0, 0); // Get the Y position of the mouse and rotate the pivot
+
+        float desiredYAngle = target.eulerAngles.y; // Move the camera based on the current rotation of the targeet and the original offset
+        float desiredXAngle = pivot.eulerAngles.x;
+
+        Quaternion rotation = Quaternion.Euler(desiredXAngle, desiredYAngle, 0); // Tracking the Cursor in Software
+        transform.position = target.position - (rotation * offset);
+
+        // transform.position = target.position - offset; // Changing the camera position and offset depending on the Character's position
+
+        if(transform.position.y < target.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y - 0.5f, transform.position.z); 
+        }
+
+        transform.LookAt(target); // Focus camera on Character
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Variables
+    public float moveSpeed;
+    public float jumpHeight;
+    public CharacterController CC;
+    
+    private Vector3 moveDirection;
+    public float gravityScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CC = GetComponent<CharacterController>(); // As soon as the Game starts, find Component "CharacterController"
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, moveDirection.y, Input.GetAxis("Vertical") * moveSpeed); // Obtain the CharacterController (CC) Horizontal Axis (X Value) and Vertical Axis (Z Value) multiply it's default move speed (1) to our set 16 in Unity
+
+        float yStore = moveDirection.y;
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical") * moveSpeed) + (transform.right * Input.GetAxisRaw("Horizontal") * moveSpeed); // Same input as above however it listens to the camera's properties
+        // moveDirection = moveDirection.normalized * moveSpeed; // Prevents two inputs from increasing speed accidently
+        moveDirection.y = yStore;
+
+
+        if (CC.isGrounded) // Check if Player is touching a floor surface
+        {
+            moveDirection.y = 0f; // Allows a more realistic fall rather then a straight dive downward
+        if (Input.GetButtonDown("Jump")) // Ask Unity if the Jump Alias is being pressed
+            { 
+            moveDirection.y = jumpHeight; // Balancing the Y Character Axis to the JumpHeight Value
+            }
+        }
+
+        moveDirection.y += (Physics.gravity.y * gravityScale * Time.deltaTime); // Equal jumpHeight to the Gravity Value set in Unity, adding in correct gravity and jumping physics (No need for DeltaTime due to the next line using DeltaTime)
+        CC.Move(moveDirection * Time.deltaTime); // Times the MoveDirection by Delta Time to stabilize the movement correctly to all clients (ALWAYS USE DELTATIME TO STABILIZE CLIENT MOVEMENT!)
+    }
+}
